2fe9ccf1958ea0801dec3b6b3a7c653d41cc6a3e
calico-dev <vincentodowd@proton.me>
2025-12-12 13:40:53 -0500

    built simple UI

--- calculator/Form1.Designer.cs
+++ calculator/Form1.Designer.cs
@@ -28,12 +28,307 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            equals = new Button();
+            dot = new Button();
+            zero = new Button();
+            posneg = new Button();
+            one = new Button();
+            two = new Button();
+            three = new Button();
+            six = new Button();
+            five = new Button();
+            four = new Button();
+            seven = new Button();
+            eight = new Button();
+            nine = new Button();
+            plus = new Button();
+            subtract = new Button();
+            multiply = new Button();
+            divide = new Button();
+            back = new Button();
+            clearall = new Button();
+            squareroot = new Button();
+            square = new Button();
+            reciprocal = new Button();
+            clearentry = new Button();
+            percent = new Button();
+            SuspendLayout();
+            // 
+            // equals
+            // 
+            equals.Location = new Point(253, 384);
+            equals.Name = "equals";
+            equals.Size = new Size(76, 49);
+            equals.TabIndex = 0;
+            equals.Text = "=";
+            equals.UseVisualStyleBackColor = true;
+            // 
+            // dot
+            // 
+            dot.Location = new Point(171, 383);
+            dot.Name = "dot";
+            dot.Size = new Size(76, 50);
+            dot.TabIndex = 1;
+            dot.Text = ".";
+            dot.UseVisualStyleBackColor = true;
+            // 
+            // zero
+            // 
+            zero.Location = new Point(89, 384);
+            zero.Name = "zero";
+            zero.Size = new Size(76, 50);
+            zero.TabIndex = 2;
+            zero.Text = "0";
+            zero.UseVisualStyleBackColor = true;
+            // 
+            // posneg
+            // 
+            posneg.Location = new Point(7, 383);
+            posneg.Name = "posneg";
+            posneg.Size = new Size(76, 50);
+            posneg.TabIndex = 3;
+            posneg.Text = "+/-";
+            posneg.UseVisualStyleBackColor = true;
+            // 
+            // one
+            // 
+            one.Location = new Point(7, 327);
+            one.Name = "one";
+            one.Size = new Size(76, 50);
+            one.TabIndex = 4;
+            one.Text = "1";
+            one.UseVisualStyleBackColor = true;
+            // 
+            // two
+            // 
+            two.Location = new Point(89, 327);
+            two.Name = "two";
+            two.Size = new Size(76, 50);
+            two.TabIndex = 5;
+            two.Text = "2";
+            two.UseVisualStyleBackColor = true;
+            // 
+            // three
+            // 
+            three.Location = new Point(171, 327);
+            three.Name = "three";
+            three.Size = new Size(76, 50);
+            three.TabIndex = 6;
+            three.Text = "3";
+            three.UseVisualStyleBackColor = true;
+            // 
+            // six
+            // 
+            six.Location = new Point(171, 271);
+            six.Name = "six";
+            six.Size = new Size(76, 50);
+            six.TabIndex = 7;
+            six.Text = "6";
+            six.UseVisualStyleBackColor = true;
+            // 
+            // five
+            // 
+            five.Location = new Point(89, 271);
+            five.Name = "five";
+            five.Size = new Size(76, 50);
+            five.TabIndex = 8;
+            five.Text = "5";
+            five.UseVisualStyleBackColor = true;
+            // 
+            // four
+            // 
+            four.Location = new Point(7, 271);
+            four.Name = "four";
+            four.Size = new Size(76, 50);
+            four.TabIndex = 9;
+            four.Text = "4";
+            four.UseVisualStyleBackColor = true;
+            // 
+            // seven
+            // 
+            seven.Location = new Point(7, 215);
+            seven.Name = "seven";
+            seven.Size = new Size(76, 50);
+            seven.TabIndex = 10;
+            seven.Text = "7";
+            seven.UseVisualStyleBackColor = true;
+            // 
+            // eight
+            // 
+            eight.Location = new Point(89, 215);
+            eight.Name = "eight";
+            eight.Size = new Size(76, 50);
+            eight.TabIndex = 11;
+            eight.Text = "8";
+            eight.UseVisualStyleBackColor = true;
+            // 
+            // nine
+            // 
+            nine.Location = new Point(171, 215);
+            nine.Name = "nine";
+            nine.Size = new Size(76, 50);
+            nine.TabIndex = 12;
+            nine.Text = "9";
+            nine.UseVisualStyleBackColor = true;
+            // 
+            // plus
+            // 
+            plus.Location = new Point(253, 328);
+            plus.Name = "plus";
+            plus.Size = new Size(76, 50);
+            plus.TabIndex = 13;
+            plus.Text = "+";
+            plus.UseVisualStyleBackColor = true;
+            // 
+            // subtract
+            // 
+            subtract.Location = new Point(253, 271);
+            subtract.Name = "subtract";
+            subtract.Size = new Size(76, 50);
+            subtract.TabIndex = 14;
+            subtract.Text = "-";
+            subtract.UseVisualStyleBackColor = true;
+            // 
+            // multiply
+            // 
+            multiply.Location = new Point(253, 216);
+            multiply.Name = "multiply";
+            multiply.Size = new Size(76, 50);
+            multiply.TabIndex = 15;
+            multiply.Text = "x";
+            multiply.UseVisualStyleBackColor = true;
+            // 
+            // divide
+            // 
+            divide.Location = new Point(253, 160);
+            divide.Name = "divide";
+            divide.Size = new Size(76, 50);
+            divide.TabIndex = 16;
+            divide.Text = "÷";
+            divide.UseVisualStyleBackColor = true;
+            // 
+            // back
+            // 
+            back.Location = new Point(253, 104);
+            back.Name = "back";
+            back.Size = new Size(76, 50);
+            back.TabIndex = 17;
+            back.Text = "←";
+            back.UseVisualStyleBackColor = true;
+            // 
+            // clearall
+            // 
+            clearall.Location = new Point(171, 104);
+            clearall.Name = "clearall";
+            clearall.Size = new Size(76, 50);
+            clearall.TabIndex = 18;
+            clearall.Text = "C";
+            clearall.UseVisualStyleBackColor = true;
+            // 
+            // squareroot
+            // 
+            squareroot.Location = new Point(171, 160);
+            squareroot.Name = "squareroot";
+            squareroot.Size = new Size(76, 50);
+            squareroot.TabIndex = 19;
+            squareroot.Text = "2√x";
+            squareroot.UseVisualStyleBackColor = true;
+            // 
+            // square
+            // 
+            square.Location = new Point(89, 160);
+            square.Name = "square";
+            square.Size = new Size(76, 50);
+            square.TabIndex = 20;
+            square.Text = "x²";
+            square.UseVisualStyleBackColor = true;
+            // 
+            // reciprocal
+            // 
+            reciprocal.Location = new Point(7, 160);
+            reciprocal.Name = "reciprocal";
+            reciprocal.Size = new Size(76, 50);
+            reciprocal.TabIndex = 21;
+            reciprocal.Text = "1/x";
+            reciprocal.UseVisualStyleBackColor = true;
+            // 
+            // clearentry
+            // 
+            clearentry.Location = new Point(89, 104);
+            clearentry.Name = "clearentry";
+            clearentry.Size = new Size(76, 50);
+            clearentry.TabIndex = 22;
+            clearentry.Text = "CE";
+            clearentry.UseVisualStyleBackColor = true;
+            // 
+            // percent
+            // 
+            percent.Location = new Point(7, 104);
+            percent.Name = "percent";
+            percent.Size = new Size(76, 50);
+            percent.TabIndex = 23;
+            percent.Text = "%";
+            percent.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(347, 445);
+            Controls.Add(percent);
+            Controls.Add(clearentry);
+            Controls.Add(reciprocal);
+            Controls.Add(square);
+            Controls.Add(squareroot);
+            Controls.Add(clearall);
+            Controls.Add(back);
+            Controls.Add(divide);
+            Controls.Add(multiply);
+            Controls.Add(subtract);
+            Controls.Add(plus);
+            Controls.Add(nine);
+            Controls.Add(eight);
+            Controls.Add(seven);
+            Controls.Add(four);
+            Controls.Add(five);
+            Controls.Add(six);
+            Controls.Add(three);
+            Controls.Add(two);
+            Controls.Add(one);
+            Controls.Add(posneg);
+            Controls.Add(zero);
+            Controls.Add(dot);
+            Controls.Add(equals);
+            Name = "Form1";
+            Text = "Calculator";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button equals;
+        private Button dot;
+        private Button zero;
+        private Button posneg;
+        private Button one;
+        private Button two;
+        private Button three;
+        private Button six;
+        private Button five;
+        private Button four;
+        private Button seven;
+        private Button eight;
+        private Button nine;
+        private Button plus;
+        private Button subtract;
+        private Button multiply;
+        private Button divide;
+        private Button back;
+        private Button clearall;
+        private Button squareroot;
+        private Button square;
+        private Button reciprocal;
+        private Button clearentry;
+        private Button percent;
     }
 }
